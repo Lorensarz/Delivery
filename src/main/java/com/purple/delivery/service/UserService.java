57088adbc8d1801287332a5338542be14cb608bc
lorens999 <lorens999@yandex.ru>
2023-10-31 10:46:11 +0300

    add service

---
+++ src/main/java/com/purple/delivery/service/UserService.java
@@ -0,0 +1,10 @@
+package com.purple.delivery.service;
+
+import com.purple.delivery.model.Courier;
+
+public class UserService {
+    public Courier findCourier() {
+        Courier courier = new Courier();
+        return courier;
+    }
+}
