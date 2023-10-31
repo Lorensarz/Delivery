ecf628a90669364fa33ab0b96b691af973ea5a4c
lorens999 <lorens999@yandex.ru>
2023-10-31 10:11:57 +0300

    add changes Delivery class

--- src/main/java/com/purple/delivery/model/Delivery.java
+++ src/main/java/com/purple/delivery/model/Delivery.java
@@ -1,26 +1,33 @@
 package com.purple.delivery.model;
 
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Delivery {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    UUID delivery_uuid;
-    UUID order_uuid;
-    Date order_date;
-    LocalDateTime delivery_date;
-    OrderStatus Status;
-    UUID curier;
-    String adress;
-    BigDecimal cost;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID delivery_uuid;
+    private UUID order_uuid;
+    private Date order_date;
+    private LocalDateTime delivery_date;
+    private OrderStatus Status;
+    private UUID curier;
+    private String adress;
+    private BigDecimal cost;
 
 
 }
