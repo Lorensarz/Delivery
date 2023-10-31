129e513970f26c60764634f38b35fe2b6df3724a
Vladimir Bobrovskii <bobrovskiivs@gmail.com>
2023-10-31 09:42:01 +0400

    add repo

--- src/main/java/com/purple/delivery/model/Delivery.java
+++ src/main/java/com/purple/delivery/model/Delivery.java
@@ -1,10 +1,16 @@
 package com.purple.delivery.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Delivery {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     UUID delivery_uuid;
     UUID order_uuid;
     Date date;
