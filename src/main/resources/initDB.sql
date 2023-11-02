b2769d0101f1d6f99554eaa249b0b574a44171d3
Vladimir Bobrovskii <bobrovskiivs@gmail.com>
2023-11-02 12:13:41 +0400

    init script added

---
+++ src/main/resources/initDB.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE delivery;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+create type order_status as enum ('new', 'processing', 'shipped', 'delivered', 'cancelled');
+
+
+CREATE TABLE delivery(
+uuid uuid  DEFAULT gen_random_uuid() PRIMARY KEY,
+  order_uuid uuid,
+ order_date TIMESTAMP ,
+ delivery_date TIMESTAMP ,
+ orderState order_status ,
+ curier uuid ,
+ adress VARCHAR  ,
+ cost numeric
+)
+
+SELECT * FROM delivery
