ce8a014ec10bc5e4e70c33720f95f450180a084a
lorens999 <lorens999@yandex.ru>
2023-11-08 11:54:27 +0300

    fix DeliveryController

--- src/main/java/com/purple/delivery/controller/DeliveryController.java
+++ src/main/java/com/purple/delivery/controller/DeliveryController.java
@@ -1,106 +1,64 @@
-//package com.purple.delivery.controller;
-//
-//import com.purple.delivery.dto.DeliveryDto;
-//import com.purple.delivery.dto.OrderDto;
-//import com.purple.delivery.model.OrderStatus;
-//import com.purple.delivery.service.DeliveryService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.RequestEntity;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.util.UriComponentsBuilder;
-//
-//import java.math.BigDecimal;
-//import java.net.URI;
-//import java.time.LocalDateTime;
-//import java.util.UUID;
-//
-//@RestController
-//@RequestMapping("/delivery")
-//@SessionAttributes("delivery")
-//public class DeliveryController {
-//
-//    @Value("${findCourierUrl}")
-//    String urlCourier;
-//
-//
-//    @Value("${getAddressUrl}")
-//    String urlAddress;
-//
-//    private final DeliveryService deliveryService;
-//
-//    @Autowired
-//    public DeliveryController(DeliveryService deliveryService) {
-//        this.deliveryService = deliveryService;
-//
-//    }
-//
-//    @PostMapping("/createDelivery")
-//    public ResponseEntity<String> createDelivery(@RequestBody OrderDto orderDto) {
-//        UUID uuid = orderDto.getUuid();
-//        String address = orderDto.getAddress();
-//        DeliveryDto deliveryDto = new DeliveryDto();
-//        deliveryDto.setOrder_uuid(uuid);
-//        deliveryDto.setAddress(address);
-//        deliveryDto.setDelivery_date(LocalDateTime.now().plusDays(1));
-//        deliveryDto.setOrder_date(orderDto.getTimestamp().toLocalDateTime());
-//        deliveryDto.setCost(new BigDecimal(500));
-//        deliveryDto.setOrderstate(OrderStatus.NEW);
-//        deliveryService.create(deliveryDto);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(deliveryDto.toString());
-//    }
-//
-////    @GetMapping("/findCourier")
-////    @ResponseStatus(HttpStatus.ACCEPTED)
-////    public ResponseEntity<String> findCourier(@RequestParam UUID uuid) {
-////        DeliveryDto delivery = deliveryService.findByUUID(uuid); //??
-////        if (delivery == null) {
-////            return ResponseEntity.status(HttpStatus.CONFLICT).body("Delivery not found");
-////        }
-////        RestTemplate restTemplate = new RestTemplate();
-////        URI uri = UriComponentsBuilder.fromUriString(urlCourier).build(42);
-////        RequestEntity<Void> requestEntity = RequestEntity.get(uri)
-////                .build();
-////        ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
-////        String courierId = response.getHeaders().getFirst("courierId");
-////        delivery.setCourier(UUID.fromString(courierId));
-////        deliveryService.processOrder(delivery);
-////        return ResponseEntity.status(HttpStatus.ACCEPTED).body(courierId);
-////    }
-//
-//    @PostMapping("/findCourier")
-//    public ResponseEntity<String> findCourier(@RequestHeader("userId") UUID courier_uuid) {
-//        return null;
-//
-//    }
-//
-//    @GetMapping("/getAddress")
-//    @ResponseStatus(HttpStatus.OK)
-//    public ResponseEntity<String> getDeliveryAddress(@RequestParam UUID uuid) {
-//        DeliveryDto delivery = deliveryService.findByUUID(uuid);
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        URI uri = UriComponentsBuilder.fromUriString(urlAddress).build(42);
-//        RequestEntity<Void> requestEntity = RequestEntity.get(uri)
-//                .header("userId", delivery.getClientId().toString())
-//                .build();
-//        ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
-//        String address = response.getHeaders().getFirst("address");
-//        delivery.setAddress(address);
-//        delivery.setOrderstate(OrderStatus.PROCESSING);
-//        deliveryService.processOrder(delivery);
-//        return ResponseEntity.status(HttpStatus.OK).body("Address set");
-//    }
-//
-//
-//    @GetMapping("/findAll")
-//    @ResponseStatus(HttpStatus.OK)
-//    public ResponseEntity<Iterable<DeliveryDto>> findAll() {
-//        Iterable<DeliveryDto> deliveryDtos = deliveryService.findAll();
-//        return new ResponseEntity<>(deliveryDtos, HttpStatus.OK);
-//    }
-//
-//}
+package com.purple.delivery.controller;
+
+
+import com.purple.delivery.dto.DeliveryDto;
+import com.purple.delivery.dto.OrderDto;
+import com.purple.delivery.model.OrderStatus;
+import com.purple.delivery.service.DeliveryService;
+import com.purple.delivery.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/delivery")
+@Tag(name = "Сервис доставки", description = "Операции, связанные с доставкой.")
+public class DeliveryController {
+
+    private final DeliveryService deliveryService;
+    private final UserService userService;
+    private final DeliveryDto deliveryDto = new DeliveryDto();
+
+    public DeliveryController(DeliveryService deliveryService, UserService userService) {
+        this.deliveryService = deliveryService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/createDelivery")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Создать заказ на доставку", description = "Создает доставку в базе данных")
+    @ApiResponse(responseCode = "201", description = "create delivery")
+    public ResponseEntity<String> createDelivery(@RequestBody OrderDto orderDto) {
+        UUID orderUuid = orderDto.getUuid();
+        UUID clientId = orderDto.getClientId();
+        String address = userService.fetchAddressFromRemoteUserService(clientId.toString());
+        UUID courierUuid = userService.fetchCourierFromRemoteUserService();
+
+        deliveryDto.setOrderUuid(orderUuid);
+        deliveryDto.setAddress(address);
+        deliveryDto.setCourier(courierUuid);
+        deliveryDto.setDeliveryDate(LocalDateTime.now().plusDays(1));
+        deliveryDto.setOrderDate(orderDto.getTimestamp().toLocalDateTime());
+        deliveryDto.setCost(new BigDecimal(500));
+        deliveryDto.setOrderState(OrderStatus.NEW);
+
+        deliveryService.create(deliveryDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Order create");
+    }
+
+
+    @GetMapping("/findAll")
+    @Operation(summary = "Получить все заказы на доставку",
+            description = "Возвращает список всех заказов нп доставку")
+    public ResponseEntity<Iterable<DeliveryDto>> findAll() {
+        Iterable<DeliveryDto> deliveryDtos = deliveryService.findAll();
+        return new ResponseEntity<>(deliveryDtos, HttpStatus.OK);
+    }
+}
